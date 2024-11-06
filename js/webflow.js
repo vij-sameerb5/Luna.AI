cb96300622c85570cefe6a1f58e4f342e68d94c3
Shaik Sameer <109991523+vij-sameerb5@users.noreply.github.com>
2024-11-06 02:53:56 -0500

    Update webflow.js

--- js/webflow.js
+++ js/webflow.js
@@ -1,12 +1,4 @@
 
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 (() => {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
